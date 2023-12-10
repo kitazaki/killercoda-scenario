57555cba4984ab427df5f35bc7b86a17998ae7fd
Ayachika Kitazaki <kitazaki@gmail.com>
2023-12-11 00:01:49 +0900

    Update foreground.sh

--- node-red-playground/foreground.sh
+++ node-red-playground/foreground.sh
@@ -4,6 +4,6 @@ while [ ! -f /tmp/finished ]; do sleep 1; done
 
 source ~/.bashrc
 nvm install 18
-sudo npm install -g --unsafe-perm node-red
+npm install -g --unsafe-perm node-red
 
 nohup node-red &
