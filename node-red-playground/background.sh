81bea9b15adc664cb9438c21ac8c164a9a569aa5
Ayachika Kitazaki <kitazaki@gmail.com>
2023-12-10 23:54:25 +0900

    Update background.sh

--- node-red-playground/background.sh
+++ node-red-playground/background.sh
@@ -8,7 +8,7 @@
 #sudo apt-get update
 #sudo apt-get install nodejs -y
 
-#curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 #source ~/.bashrc
 #nvm install 18
 
