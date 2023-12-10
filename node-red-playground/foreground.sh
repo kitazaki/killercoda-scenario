b2157b1148facfefa10083e6f1d3aca261344f65
Ayachika Kitazaki <kitazaki@gmail.com>
2023-12-10 23:03:38 +0900

    Update foreground.sh

--- node-red-playground/foreground.sh
+++ node-red-playground/foreground.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
-sudo apt-get install nodejs -y
+while [ ! -f /tmp/finished ]; do sleep 1; done
 
 sudo npm install -g --unsafe-perm node-red
 
